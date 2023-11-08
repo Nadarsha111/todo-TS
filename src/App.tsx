9a2a6aa06b66edaf56b8ce96de4ed43fad226388
Nadarsha111 <nadarsha565@gmail.com>
2023-11-08 23:18:15 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+      ...
+    </button>
+  );
+};
+
+export default App;
